b207612925a9b6d6b2e0efb9d1a7db9c53eb9512
Jack <jlord25@eq.edu.au>
2018-10-20 14:57:03 +1000

    Here are new codes that have been modified to work

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicOpMode_Iterative4.java
@@ -0,0 +1,156 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+public class BasicOpMode_Iterative4 extends OpMode{
+    // Declare OpMode members.
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor liftMotor = null;
+    private Servo servo0 = null;
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        telemetry.addData("Status", "Initialized");
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrive  = hardwareMap.get(DcMotor.class, "MotorLeft");
+        rightDrive = hardwareMap.get(DcMotor.class, "MotorRight");
+        liftMotor = hardwareMap.get(DcMotor.class, "MotorLift");
+        servo0 = hardwareMap.get(Servo.class, "servo0");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+////////////////////
+
+
+        // Wait for the start button
+        telemetry.addData(">", "Press Start" );
+        telemetry.update();
+
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        runtime.reset();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        // Setup a variable for each drive wheel to save power level for telemetry
+        double leftPower;
+        double rightPower;
+
+        // Choose to drive using either Tank Mode, or POV Mode
+        // Comment out the method that's not used.  The default below is POV.
+
+        // POV Mode uses left stick to go forward, and right stick to turn.
+        // - This uses basic math to combine motions and is easier to drive straight.
+
+        //POV mode
+        /*
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+        */
+
+        //Tank mode
+        // Tank Mode uses one stick to control each wheel.
+        // - This requires no math, but it is hard to drive forward slowly and keep straight.
+        leftPower  = gamepad1.left_stick_y ;
+        rightPower = gamepad1.right_stick_y ;
+
+        // Send calculated power to wheels
+        leftDrive.setPower(leftPower);
+        rightDrive.setPower(rightPower);
+
+
+
+        //////////////////////////////////////////////////
+
+
+
+        String liftMotorStatus;
+        //this gives the status of the motor to the phone
+        if(gamepad1.dpad_down) {
+
+            liftMotor.setPower(-1);
+            liftMotorStatus = "retracting";
+        }
+        else if (gamepad1.dpad_up) {
+
+            liftMotor.setPower(1);
+            liftMotorStatus = "extending";
+        }
+        else{
+
+            liftMotor.setPower(0);
+            liftMotorStatus = "Still";
+        }
+
+        //////////////////////////////////////////////////////////////
+        String markerStatus = "";
+        if(gamepad1.b){
+            servo0.setPosition(0);
+            markerStatus = "0";
+        }
+        if(gamepad1.a){
+            servo0.setPosition(0.5);
+            markerStatus = "90";
+        }
+        if(gamepad1.x){
+            servo0.setPosition(1);
+            markerStatus = "180";
+        }
+
+        // Show the elapsed game time and wheel power.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        telemetry.addData("Lift", liftMotorStatus);
+        telemetry.addData("MarkerServo", markerStatus);
+
+        telemetry.addData("Status", "Running");
+        telemetry.update();
+
+
+    }
+
+    /*f
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
